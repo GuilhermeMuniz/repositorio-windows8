ca4619f68e74319b9f090e287d2dba43514a380c
Guilherme <guilherme.cmuniz@gmail.com>
2014-05-26 21:41:00 -0300

    Commit inicial arquivo java

---
+++ Pessoa.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Guilherme Muniz
+ */
+public class Pessoa {
+
+    private String nome;
+    private int cpf;
+    private int rg; 
+    private int idade;
+
+    public Pessoa(){
+        
+    }
+    
+    
+    
+    public Pessoa(String nome, int cpf, int rg, int idade){
+        
+        this.cpf = cpf;
+        this.idade = idade;
+        this.rg = rg;
+        this.nome = nome;
+        
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public int getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the rg
+     */
+    public int getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+    
+    
+    
+}
